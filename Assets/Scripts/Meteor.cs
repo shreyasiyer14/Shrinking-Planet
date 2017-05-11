148c7c03f0641c55bc5653effc81f31ddd452187
Shreyas Iyer <shreyasiyer14@gmail.com>
2017-05-11 22:21:59 +0530

    Added a primitive meteor spawner script.. Need to tweak the radius settings for the perfect spawning of the meteors

---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Meteor : MonoBehaviour {
+	[SerializeField] private float timeForSpawn;
+	[SerializeField] private GameObject meteor;
+	[SerializeField] private float planetRadius;
+
+	private int index = 0;
+	void Start () {
+		InvokeRepeating ("spawnMeteor", 0.1f, timeForSpawn);
+	}
+	
+	void spawnMeteor () {
+		Vector3 pos = Random.onUnitSphere * planetRadius;
+		Quaternion rot = Quaternion.FromToRotation (Vector3.forward, transform.position - pos);
+		Instantiate (meteor, pos, rot);
+	}
+}
