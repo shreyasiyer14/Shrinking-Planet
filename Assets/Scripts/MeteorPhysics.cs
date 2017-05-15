c0dd1ab926073c73bc8f8aa95a4fe6e07b486c9c
Shreyas Iyer <shreyasiyer14@gmail.com>
2017-05-15 14:21:06 +0530

    Added game over conditions + conditions for meteor squashing on hitting the planet.

--- Assets/Scripts/MeteorPhysics.cs
+++ Assets/Scripts/MeteorPhysics.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MeteorPhysics : MonoBehaviour {
-
+	[SerializeField] private GameObject hitMeteorPrefab;
 	private GameObject gravityCentre;
 
 	void Start () {
@@ -14,5 +14,18 @@ public class MeteorPhysics : MonoBehaviour {
 
 	void Update () {
 		GetComponent<Rigidbody>().AddForce((gravityCentre.transform.position - transform.position).normalized * 25f);
+	}
+
+	void OnCollisionEnter (Collision hit) {
+		if (hit.gameObject.tag == "Planet") {
+			GetComponent<Rigidbody> ().detectCollisions = false;
+			GetComponent<Rigidbody> ().isKinematic = true;
+			SwitchModel ();
+		}
+	}
+
+	void SwitchModel () {
+		Destroy (gameObject);
+		Instantiate(hitMeteorPrefab, transform.position, Quaternion.FromToRotation (Vector3.forward, transform.position - gravityCentre.transform.position));
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,13 +19,13 @@ public class PlayerController : MonoBehaviour {
 
 	void Update () {
 		RotateAroundPlanet ();
+
 		#region INPUT_MANAGER
 		if (Input.GetKey(KeyCode.D))
 			transform.Rotate (new Vector3 (turnFactor, 0f, 0f));
 
 		if (Input.GetKey(KeyCode.A))
 			transform.Rotate (new Vector3 (-turnFactor, 0f, 0f));
-
 		#endregion
 	}
 
@@ -34,7 +34,10 @@ public class PlayerController : MonoBehaviour {
 		GetComponent<Rigidbody>().AddForce((planet.position - transform.position).normalized * turnFactor * 15f);
 	}
 
-	void OnCollisionEnter () {
-
+	void OnCollisionEnter (Collision hit) {
+		if (hit.gameObject.tag == "Meteor") {
+			Debug.Log ("Game Over!");
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
 	}
 }
