d7b115c07ffb498fe999be1984675971dfd0ce3a
Shreyas Iyer <shreyasiyer14@gmail.com>
2017-05-23 12:28:34 +0530

    Added planet scale reduction script.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField] private Transform planet;
+	[SerializeField] private GameObject UX;
+	[SerializeField] private float reductionFactor;
+	void Update () {
+		ReducePlanetScale ();
+		Debug.Log ("Score: " +  planet.localScale + "m");
+	}
+
+	void ReducePlanetScale () {
+		planet.localScale = new Vector3 (planet.localScale.x - reductionFactor * Time.fixedDeltaTime, planet.localScale.y - reductionFactor * Time.fixedDeltaTime, planet.localScale.z - reductionFactor * Time.fixedDeltaTime);
+	}
+}
